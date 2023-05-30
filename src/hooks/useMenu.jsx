6fe1fd7a3eea534ca3b89bb8f78214525d08f0d9
titumiranan123 <titumiranan.gtc@gmail.com>
2023-05-30 14:59:51 +0600

    all code complete but admin section is ongoing

---
+++ src/hooks/useMenu.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useMenu = () => {
+    const [menu, setMenu] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch('https://resturent-server-side.vercel.app/menu')
+            .then(res => res.json())
+            .then(data => {
+                setMenu(data);
+                setLoading(false);
+            });
+    }, [])
+    return [menu, loading]
+}
+
+export default useMenu;
